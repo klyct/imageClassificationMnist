82ac5d38db4a25fbed55ec9e78ae7030c96bfb3a
Kimberly <kimberly.cantu@cidecuc.mx>
2024-03-22 21:34:44 -0600

    ModeloMNist

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import numpy as np
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# cargamos conjunto de datos
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+# normalizamos los datos
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# construccion del modelo
+# dedinimoa la arquitectura del modelo de la red neuronal (cnn)
+model = models.Sequential([
+    # es la primera capa convolucional, tiene 32 filtros y es de 3x3
+    # espera imagenes de 28x28 en escala de grises
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    # aplicamos una capa de aprupamiento maximum para reducir dimensionalidad
+    # manteniendo las caracteristicas imporantes
+    layers.MaxPooling2D((2, 2)),
+    # una sefunda capa convolucinal
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    # volvemos a agrupar
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    # esta acapa aolana la salida para alimentar una capa densa
+    layers.Flatten(),
+    # es una capa densa se 64 neuronas
+    layers.Dense(64, activation='relu'),
+    # capa de salida con 10 neuronas (una para cada digito)
+    # softman es para clasificacion multiclase
+    layers.Dense(10, activation='softmax')
+])
 
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Entrenamiento del modelo
+model.fit(x_train.reshape(-1, 28, 28, 1), y_train, epochs=5, batch_size=64)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Evaluacion del modelo
+test_loss, test_acc = model.evaluate(x_test.reshape(-1, 28, 28, 1), y_test)
+print(f'Accuracy: {test_acc}')
